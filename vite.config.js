692509db17db91b163094ed421135f560442447d
Cuong123 <vuong.tieucuong@gmail.com>
2025-03-07 13:41:22 +0700

    hello

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './thuando-portfolio/' // This ensures assets are loaded correctly on GitHub Pages
+})
